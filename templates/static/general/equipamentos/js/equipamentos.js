c353e27c2942a4e9a6f9b59f789d92a0bd3380e1
Bowie <61563289+alanproenca@users.noreply.github.com>
2023-11-23 18:54:40 -0300

    primeira comitada

---
+++ templates/static/general/equipamentos/js/equipamentos.js
@@ -0,0 +1,7 @@
+function add_equipamento() {
+
+    container = document.getElementById('form-equipamento')
+
+    html = "<br>  <div class='row'> <div class= 'col-md'> <imput type='text' placeholder = 'equipamento' class= 'form-control' name='equipamento'> </div> </div>"
+
+}
